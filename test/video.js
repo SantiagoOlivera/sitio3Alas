ccb8d3f2100c296265d8950b8c357fba2f5d4f96
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2019-05-21 21:48:04 -0300

    videos,audios,supports,filters, clip-path, semantic, ect

---
+++ test/video.js
@@ -0,0 +1,16 @@
+reproducir.addEventListener(
+    "click",
+    function(){
+        console.log("hiciste click");
+        musica.play();
+    }
+);
+
+detener.addEventListener(
+    "click",
+    function(){
+        console.log("hiciste click wachin");
+        musica.pause();
+    }
+);
+
