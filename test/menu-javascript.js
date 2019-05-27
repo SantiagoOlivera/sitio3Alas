34a1863b05aab817998c11f2799fd488ab1dae3a
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2019-05-26 22:08:50 -0300

    evento detectar click en elemento li en menu ul

--- test/menu-javascript.js
+++ test/menu-javascript.js
@@ -1,1 +1,15 @@
-Array.from(document.querySelectorAll('item'))
+menu.addEventListener(
+    // va a escuchar el evento click
+    "click", 
+    function(){
+        console.log("Hiciste click");
+        var selectedItem = document.getElementsByClassName("item");
+        var listItems = document.getElementsByClassName("item");
+
+        for(var i=0; i<listItems.length; i++){
+            console.log("item" + listItems[i].textContent);
+            
+        }
+    }
+    
+);
